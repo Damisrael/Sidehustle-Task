9734d266dedfdb3031c47f37c5ab01c30dee9b52
Damisrael <76260434+Damisrael@users.noreply.github.com>
2021-06-26 23:55:36 +0100

    Add files via upload

---
+++ Task.php
@@ -0,0 +1,7 @@
+<?php
+///// sum function
+function sum($num1, $num2){
+return $num1 + $num2;
+echo sum(40,60);
+}
+?>
---
+++ myfile.php
@@ -0,0 +1,9 @@
+<?php
+/////// range function
+function process($start, $end){
+    return $start & $end;
+}
+for ($start = 0; $start <= 5; $start++);"<br>";
+for ($end = 6; $end <= 10; $end++);
+var_dump ($start, $end);
+?>
